6e89ec0cabadad724418446fd9fee6060cad5d3f
MiguelAP6 <miguel.paranhos.2025073@my.istec.pt>
2026-05-18 10:43:48 +0100

    criação do modelo filme e configuração do admin

--- catalogo/filmes/models.py
+++ catalogo/filmes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Filme(models.Model):
+    titulo = models.CharField(max_length=200)
+    ano = models.IntegerField()
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.titulo
